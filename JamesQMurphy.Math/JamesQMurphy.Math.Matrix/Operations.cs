a287659b285078f0b4b9340e1be1ba801ea65b4d
James Murphy <JamesQMurphy@yahoo.com>
2019-03-03 22:41:54 -0500

    Added Operations class + tests; Add/Multiply only

---
+++ JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Operations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace JamesQMurphy.Math
+{
+    public static class Operations<T> where T:struct
+    {
+        // thanks to this Stack Overflow answer:
+        // https://stackoverflow.com/a/11113450/1001100
+        private enum _binaryOperations
+        {
+            op_Addition = 0,
+            op_Subtraction,
+            op_Multiply,
+            op_Division
+        }
+
+        private static List<MethodInfo> _binaryOpMethodInfos = new List<MethodInfo>();
+
+        static Operations()
+        {
+            // Populate private list of methodinfo objects with all operations in the enum
+            foreach (string opName in Enum.GetNames(typeof(_binaryOperations)))
+            {
+                var mi = typeof(T).GetMethod(opName, BindingFlags.Static | BindingFlags.Public);
+                _binaryOpMethodInfos.Add(mi);
+            }
+        }
+
+        private static T _InvokeBinaryOperation(_binaryOperations op, T left, T right)
+        {
+            var mi = _binaryOpMethodInfos[(int)op];
+            if (mi != null)
+            {
+                return (T)mi.Invoke(null, new object[] { left, right });
+            }
+            throw new MissingMethodException(typeof(T).FullName, op.ToString());
+        }
+
+        #region Add
+        public static T Add(T left, T right)
+        {
+            return _InvokeBinaryOperation(_binaryOperations.op_Addition, left, right);
+        }
+
+        public static int Add(int left, int right) { return left + right; }
+        public static double Add(double left, double right) { return left + right; }
+        #endregion
+
+        #region Multiply
+        public static T Multiply(T left, T right)
+        {
+            return _InvokeBinaryOperation(_binaryOperations.op_Multiply, left, right);
+        }
+        public static int Multiply(int left, int right) { return left * right; }
+        public static double Multiply(double left, double right) { return left * right; }
+        #endregion
+    }
+}
---
+++ JamesQMurphy.Math/JamesQMurphy.Math.UnitTests/OperationsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using JamesQMurphy.Math;
+
+namespace JamesQMurphy.Math.UnitTests
+{
+    public class OperationsTests
+    {
+
+        [Test]
+        public void AddIntegers()
+        {
+            Assert.AreEqual(10, Operations<int>.Add(7, 3));
+        }
+
+        [Test]
+        public void AddDoubles()
+        {
+            Assert.AreEqual(10d, Operations<double>.Add(7d, 3d));
+        }
+
+        [Test]
+        public void AddDecimals()
+        {
+            Assert.AreEqual(10m, Operations<decimal>.Add(7m, 3m));
+        }
+
+        [Test]
+        public void MultiplyIntegers()
+        {
+            Assert.AreEqual(1271, Operations<int>.Multiply(31, 41));
+        }
+
+        [Test]
+        public void MultiplyDoubles()
+        {
+            Assert.AreEqual(1271d, Operations<double>.Multiply(31d, 41d));
+        }
+
+        [Test]
+        public void MultiplyDecimals()
+        {
+            Assert.AreEqual(1271m, Operations<decimal>.Multiply(31m, 41m));
+        }
+    }
+}
