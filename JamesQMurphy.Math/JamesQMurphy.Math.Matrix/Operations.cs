1e2fffe036579f37bdc7121f94f87e3e7c899fc5
James Murphy <jamurphy@arifleet.com>
2019-03-04 12:36:10 -0500

    Renamed operations tests; added Int16, Int64, and float addition and multiplication

--- JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Operations.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Operations.cs
@@ -24,7 +24,7 @@ namespace JamesQMurphy.Math
             foreach (string opName in Enum.GetNames(typeof(_binaryOperations)))
             {
                 var mi = typeof(T).GetMethod(opName, BindingFlags.Static | BindingFlags.Public);
-                _binaryOpMethodInfos.Add(mi);
+                _binaryOpMethodInfos.Add(mi); // Note: can be null
             }
         }
 
@@ -44,7 +44,10 @@ namespace JamesQMurphy.Math
             return _InvokeBinaryOperation(_binaryOperations.op_Addition, left, right);
         }
 
-        public static int Add(int left, int right) { return left + right; }
+        public static System.Int16 Add(System.Int16 left, System.Int16 right) { return (System.Int16)(left + right); }
+        public static System.Int32 Add(System.Int32 left, System.Int32 right) { return left + right; }
+        public static System.Int64 Add(System.Int64 left, System.Int64 right) { return left + right; }
+        public static float Add(float left, float right) { return left + right; }
         public static double Add(double left, double right) { return left + right; }
         #endregion
 
@@ -53,7 +56,10 @@ namespace JamesQMurphy.Math
         {
             return _InvokeBinaryOperation(_binaryOperations.op_Multiply, left, right);
         }
-        public static int Multiply(int left, int right) { return left * right; }
+        public static System.Int16 Multiply(System.Int16 left, System.Int16 right) { return (System.Int16)(left * right); }
+        public static System.Int32 Multiply(System.Int32 left, System.Int32 right) { return left * right; }
+        public static System.Int64 Multiply(System.Int64 left, System.Int64 right) { return left * right; }
+        public static float Multiply(float left, float right) { return left * right; }
         public static double Multiply(double left, double right) { return left * right; }
         #endregion
     }
--- JamesQMurphy.Math/JamesQMurphy.Math.UnitTests/OperationsTests.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.UnitTests/OperationsTests.cs
@@ -7,37 +7,73 @@ namespace JamesQMurphy.Math.UnitTests
     {
 
         [Test]
-        public void AddIntegers()
+        public void add_int16()
         {
-            Assert.AreEqual(10, Operations<int>.Add(7, 3));
+            Assert.AreEqual((System.Int16)10, Operations<System.Int16>.Add((System.Int16)7, (System.Int16)3));
         }
 
         [Test]
-        public void AddDoubles()
+        public void add_int32()
+        {
+            Assert.AreEqual(10, Operations<System.Int32>.Add(7, 3));
+        }
+
+        [Test]
+        public void add_int64()
+        {
+            Assert.AreEqual(10L, Operations<System.Int64>.Add(7L, 3L));
+        }
+
+        [Test]
+        public void add_float()
+        {
+            Assert.AreEqual(10f, Operations<float>.Add(7f, 3f));
+        }
+
+        [Test]
+        public void add_double()
         {
             Assert.AreEqual(10d, Operations<double>.Add(7d, 3d));
         }
 
         [Test]
-        public void AddDecimals()
+        public void add_decimal()
         {
             Assert.AreEqual(10m, Operations<decimal>.Add(7m, 3m));
         }
 
         [Test]
-        public void MultiplyIntegers()
+        public void multiply_int16()
         {
-            Assert.AreEqual(1271, Operations<int>.Multiply(31, 41));
+            Assert.AreEqual((System.Int16)1271, Operations<System.Int16>.Multiply((System.Int16)31, (System.Int16)41));
         }
 
         [Test]
-        public void MultiplyDoubles()
+        public void multiply_int32()
+        {
+            Assert.AreEqual(1271, Operations<System.Int32>.Multiply(31, 41));
+        }
+
+        [Test]
+        public void multiply_int64()
+        {
+            Assert.AreEqual(1271L, Operations<System.Int64>.Multiply(31L, 41L));
+        }
+
+        [Test]
+        public void multiply_float()
+        {
+            Assert.AreEqual(1271f, Operations<float>.Multiply(31f, 41f));
+        }
+
+        [Test]
+        public void multiply_double()
         {
             Assert.AreEqual(1271d, Operations<double>.Multiply(31d, 41d));
         }
 
         [Test]
-        public void MultiplyDecimals()
+        public void multiply_decimal()
         {
             Assert.AreEqual(1271m, Operations<decimal>.Multiply(31m, 41m));
         }
