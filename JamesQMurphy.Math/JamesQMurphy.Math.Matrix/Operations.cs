80089ce5b53a431794c7ff0b460ba0030d1d5b5e
James Murphy <JamesQMurphy@yahoo.com>
2019-03-06 23:09:28 -0500

    Streamlined operation expression trees

--- JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Operations.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Operations.cs
@@ -7,36 +7,30 @@ namespace JamesQMurphy.Math
 {
     public static class Operations<T> where T:struct
     {
-        // thanks to this Stack Overflow answer:
-        // https://stackoverflow.com/a/11113450/1001100
-        private enum _binaryOperations
-        {
-            op_Addition = 0,
-            op_Subtraction,
-            op_Multiply,
-            op_Division
-        }
-
-        private static List<MethodInfo> _binaryOpMethodInfos = new List<MethodInfo>();
+        private static readonly Delegate _delAdd;
+        private static readonly Delegate _delSub;
+        private static readonly Delegate _delMul;
+        private static readonly Delegate _delDiv;
 
         static Operations()
         {
-            // Populate private list of methodinfo objects with all operations in the enum
-            foreach (string opName in Enum.GetNames(typeof(_binaryOperations)))
-            {
-                var mi = typeof(T).GetMethod(opName, BindingFlags.Static | BindingFlags.Public);
-                _binaryOpMethodInfos.Add(mi); // Note: can be null
-            }
+            // Build expression tree lambdas for operations
+            var exprLeft = Expression.Parameter(typeof(T));
+            var exprRight = Expression.Parameter(typeof(T));
+            _delAdd = Expression.Lambda(Expression.Block(Expression.Add(exprLeft, exprRight)),      exprLeft, exprRight).Compile();
+            _delSub = Expression.Lambda(Expression.Block(Expression.Subtract(exprLeft, exprRight)), exprLeft, exprRight).Compile();
+            _delMul = Expression.Lambda(Expression.Block(Expression.Multiply(exprLeft, exprRight)), exprLeft, exprRight).Compile();
+            _delDiv = Expression.Lambda(Expression.Block(Expression.Divide(exprLeft, exprRight)),   exprLeft, exprRight).Compile();
         }
 
         // thanks to this StackOverflow answer:
         // https://stackoverflow.com/a/27584212/1001100
         private static T _CastFrom(object obj)
         {
-            var DataParam = Expression.Parameter(typeof(object), "obj");
-            var Body = Expression.Block(Expression.Convert(Expression.Convert(DataParam, obj.GetType()), typeof(T)));
-            var Run = Expression.Lambda(Body, DataParam).Compile();
-            return (T) Run.DynamicInvoke(obj);
+            var exprParam = Expression.Parameter(typeof(object));
+            var exprBlock = Expression.Block(Expression.Convert(Expression.Convert(exprParam, obj.GetType()), typeof(T)));
+            var del = Expression.Lambda(exprBlock, exprParam).Compile();
+            return (T)del.DynamicInvoke(obj);
         }
 
         #region Numeric Constants
@@ -57,50 +51,24 @@ namespace JamesQMurphy.Math
         }
         #endregion
 
-        private static T _InvokeBinaryOperation(_binaryOperations op, T left, T right)
-        {
-            var mi = _binaryOpMethodInfos[(int)op];
-            if (mi != null)
-            {
-                return (T)mi.Invoke(null, new object[] { left, right });
-            }
-            throw new MissingMethodException(typeof(T).FullName, op.ToString());
-        }
-
         public static T Add(T left, T right)
         {
-            var exprLeft = Expression.Parameter(typeof(T));
-            var exprRight = Expression.Parameter(typeof(T));
-            var body = Expression.Block(Expression.Add(exprLeft, exprRight));
-            var run = Expression.Lambda(body, exprLeft, exprRight).Compile();
-            return (T)run.DynamicInvoke(left, right);
+            return (T)_delAdd.DynamicInvoke(left, right);
         }
 
         public static T Subtract(T left, T right)
         {
-            var exprLeft = Expression.Parameter(typeof(T));
-            var exprRight = Expression.Parameter(typeof(T));
-            var body = Expression.Block(Expression.Subtract(exprLeft, exprRight));
-            var run = Expression.Lambda(body, exprLeft, exprRight).Compile();
-            return (T)run.DynamicInvoke(left, right);
+            return (T)_delSub.DynamicInvoke(left, right);
         }
 
         public static T Multiply(T left, T right)
         {
-            var exprLeft = Expression.Parameter(typeof(T));
-            var exprRight = Expression.Parameter(typeof(T));
-            var body = Expression.Block(Expression.Multiply(exprLeft, exprRight));
-            var run = Expression.Lambda(body, exprLeft, exprRight).Compile();
-            return (T)run.DynamicInvoke(left, right);
+            return (T)_delMul.DynamicInvoke(left, right);
         }
 
         public static T Divide(T left, T right)
         {
-            var exprLeft = Expression.Parameter(typeof(T));
-            var exprRight = Expression.Parameter(typeof(T));
-            var body = Expression.Block(Expression.Divide(exprLeft, exprRight));
-            var run = Expression.Lambda(body, exprLeft, exprRight).Compile();
-            return (T)run.DynamicInvoke(left, right);
+            return (T)_delDiv.DynamicInvoke(left, right);
         }
 
 
