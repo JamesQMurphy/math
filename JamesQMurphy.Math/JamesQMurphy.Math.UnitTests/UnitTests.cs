6cd5e8b03287f19ac5257a544627a5ec85e5f99d
James Murphy <JamesQMurphy@yahoo.com>
2019-03-09 16:29:14 -0500

    Introduced failing test case to kick-start development flow

--- JamesQMurphy.Math/JamesQMurphy.Math.UnitTests/UnitTests.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.UnitTests/UnitTests.cs
@@ -91,5 +91,23 @@ namespace JamesQMurphy.Math.UnitTests
             Assert.AreEqual(2.0d, uQuotient.ConversionFactor);
             Assert.AreEqual("one/two", uQuotient.Symbol);
         }
+
+        [Test]
+        public void CanDivideTwice()
+        {
+            var u1 = new Unit(1, 0, 0, 2, -3, -2, 1, 6.0d, "one");
+            var u2 = new Unit(0, 4, 1, -1, 0, -2, -1, 3.0d, "two");
+            var u3 = new Unit(1, 2, 1, -1, 4, -2, -1, 2.0d, "three");
+            var uQuotient = (u1 / u2) / u3;
+            Assert.AreEqual(0, uQuotient.UnitExponents.Length);
+            Assert.AreEqual(-6, uQuotient.UnitExponents.Mass);
+            Assert.AreEqual(-2, uQuotient.UnitExponents.Time);
+            Assert.AreEqual(4, uQuotient.UnitExponents.ElectricCurrent);
+            Assert.AreEqual(-7, uQuotient.UnitExponents.Temperature);
+            Assert.AreEqual(2, uQuotient.UnitExponents.AmountOfSubstance);
+            Assert.AreEqual(3, uQuotient.UnitExponents.LuminousIntensity);
+            Assert.AreEqual(1.0d, uQuotient.ConversionFactor);
+            Assert.AreEqual("one/(two*three)", uQuotient.Symbol);
+        }
     }
 }
