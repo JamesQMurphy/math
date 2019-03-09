a1d5430de2783d45a1b534d72250b06dc4ee2746
James Murphy <JamesQMurphy@yahoo.com>
2019-03-09 15:05:18 -0500

    Changed constructor on Units class

--- JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Units.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.Matrix/Units.cs
@@ -6,12 +6,32 @@ namespace JamesQMurphy.Math
 {
     public class Unit
     {
-        public string Symbol { get; }
         public UnitExponents UnitExponents { get; }
         public double ConversionFactor { get; }
+        public string Symbol { get; }
 
         public Unit(
-            string symbol,
+            UnitExponents unitExponents,
+            double conversionFactor
+            )
+        {
+            UnitExponents = unitExponents;
+            ConversionFactor = conversionFactor;
+            Symbol = unitExponents.ToString();
+        }
+
+        public Unit(
+            UnitExponents unitExponents,
+            double conversionFactor,
+            string symbol
+            )
+        {
+            UnitExponents = unitExponents;
+            ConversionFactor = conversionFactor;
+            Symbol = symbol;
+        }
+
+        public Unit(
             Int16 length,
             Int16 mass,
             Int16 time,
@@ -20,19 +40,29 @@ namespace JamesQMurphy.Math
             Int16 amountOfSubstance,
             Int16 luminousIntensity,
             double conversionFactor
-            )
+            ) : this(
+                new UnitExponents(length, mass, time, electricCurrent, temperature, amountOfSubstance, luminousIntensity),
+                conversionFactor
+                )
         {
-            Symbol = symbol;
-            UnitExponents = new UnitExponents(
-                length,
-                mass,
-                time,
-                electricCurrent,
-                temperature,
-                amountOfSubstance,
-                luminousIntensity
-            );
-            ConversionFactor = conversionFactor;
+        }
+
+        public Unit(
+            Int16 length,
+            Int16 mass,
+            Int16 time,
+            Int16 electricCurrent,
+            Int16 temperature,
+            Int16 amountOfSubstance,
+            Int16 luminousIntensity,
+            double conversionFactor,
+            string symbol
+            ) : this(
+                new UnitExponents(length, mass, time, electricCurrent, temperature, amountOfSubstance, luminousIntensity),
+                conversionFactor,
+                symbol
+                )
+        {
         }
 
     }
@@ -40,23 +70,23 @@ namespace JamesQMurphy.Math
     public static class Units
     {
         // SI Base Units
-        public static Unit Meter    = new Unit("m",   1, 0, 0, 0, 0, 0, 0, 1.0d);
-        public static Unit Kilogram = new Unit("kg",  0, 1, 0, 0, 0, 0, 0, 1.0d);
-        public static Unit Second   = new Unit("s",   0, 0, 1, 0, 0, 0, 0, 1.0d);
-        public static Unit Ampere   = new Unit("A",   0, 0, 0, 1, 0, 0, 0, 1.0d);
-        public static Unit Kelvin   = new Unit("K",   0, 0, 0, 0, 1, 0, 0, 1.0d);
-        public static Unit Mole     = new Unit("mol", 0, 0, 0, 0, 0, 1, 0, 1.0d);
-        public static Unit Candela  = new Unit("cd",  0, 0, 0, 0, 0, 0, 1, 1.0d);
+        public static Unit Meter    = new Unit(1, 0, 0, 0, 0, 0, 0, 1.0d, "m");
+        public static Unit Kilogram = new Unit(0, 1, 0, 0, 0, 0, 0, 1.0d, "kg");
+        public static Unit Second   = new Unit(0, 0, 1, 0, 0, 0, 0, 1.0d, "s");
+        public static Unit Ampere   = new Unit(0, 0, 0, 1, 0, 0, 0, 1.0d, "A");
+        public static Unit Kelvin   = new Unit(0, 0, 0, 0, 1, 0, 0, 1.0d, "K");
+        public static Unit Mole     = new Unit(0, 0, 0, 0, 0, 1, 0, 1.0d, "mol");
+        public static Unit Candela  = new Unit(0, 0, 0, 0, 0, 0, 1, 1.0d, "cd");
 
         // SI Prefixed units
-        public static Unit Kilometer = new Unit("km", 1, 0, 0, 0, 0, 0, 0, 0.001d);
+        public static Unit Kilometer = new Unit(1, 0, 0, 0, 0, 0, 0, 0.001d, "km");
 
         // SI Derived Units
-        public static Unit Newton = new Unit("N", 1, 1, -2, 0, 0, 0, 0, 1.0d);
+        public static Unit Newton = new Unit(1, 1, -2, 0, 0, 0, 0, 1.0d, "N");
 
         // Imperial units
-        public static Unit Foot = new Unit("ft", 1, 0, 0, 0, 0, 0, 0, 3.28084d);
-        public static Unit Inch = new Unit("in", 1, 0, 0, 0, 0, 0, 0, 1.09361d);
+        public static Unit Foot = new Unit(1, 0, 0, 0, 0, 0, 0, 3.28084d, "ft");
+        public static Unit Inch = new Unit(1, 0, 0, 0, 0, 0, 0, 1.09361d, "in");
     }
 
 
