725e8619d2e6fd636d385ba6340de9a70be354ed
James Murphy <JamesQMurphy@yahoo.com>
2019-03-10 15:12:20 -0400

    Changed implementation of UnitExponents class to use UnitParser

--- JamesQMurphy.Math/JamesQMurphy.Math.Matrix/UnitExponents.cs
+++ JamesQMurphy.Math/JamesQMurphy.Math.Matrix/UnitExponents.cs
@@ -29,74 +29,15 @@ namespace JamesQMurphy.Math
 
         private string _getUnitString()
         {
-            var sbNumerator = new StringBuilder();
-            var sbDenominator = new StringBuilder();
-            int numNumeratorTerms = 0;
-            int numDenominatorTerms = 0;
-
-            void AppendToProperStringBuilder(sbyte exponent, string symbol)
-            {
-                if (exponent < 0)
-                {
-                    sbDenominator.Append($"{_dotSymbol}{symbol}{_exponentSymbol}{exponent}");
-                    ++numDenominatorTerms;
-                }
-                if (exponent > 0)
-                {
-                    sbNumerator.Append($"{_dotSymbol}{symbol}");
-                    ++numNumeratorTerms;
-                }
-                if (exponent > 1)
-                {
-                    sbNumerator.Append($"{_exponentSymbol}{exponent}");
-                }
-            }
-
-            AppendToProperStringBuilder(_length, _meterSymbol);
-            AppendToProperStringBuilder(_mass, _kilgogramSymbol);
-            AppendToProperStringBuilder(_time, _secondsSymbol);
-            AppendToProperStringBuilder(_electricCurrent, _ampereSymbol);
-            AppendToProperStringBuilder(_temperature, _kelvinSymbol);
-            AppendToProperStringBuilder(_amountOfSubstance, _moleSymbol);
-            AppendToProperStringBuilder(_luminousIntensity, _candelaSymbol);
-
-            // Remove leading dot symbols
-            if (sbNumerator.Length > 0)
-            {
-                sbNumerator.Remove(0, 1);
-            }
-            if (sbDenominator.Length > 0)
-            {
-                sbDenominator.Remove(0, 1);
-            }
-
-            // If there's only a denominator, just return that
-            if (numNumeratorTerms == 0)
-            {
-                return sbDenominator.ToString();
-            }
-
-            if (numDenominatorTerms > 0)
-            {
-                // Since there is both a numerator and a denominator, we need to
-                // reformat the denominator by changing the signs on the exponents
-                // and removing any "^1"'s
-                sbDenominator.Replace("-", String.Empty);
-                sbDenominator.Replace($"{_exponentSymbol}1", String.Empty);
-
-                // If the denominator has more than one term, wrap it in parentheses
-                if (numDenominatorTerms > 1)
-                {
-                    sbDenominator.Insert(0, '(');
-                    sbDenominator.Append(')');
-                }
-
-                // Append the modified denominator
-                sbNumerator.Append(_solidusSymbol);
-                sbNumerator.Append(sbDenominator.ToString());
-
-            }
-            return sbNumerator.ToString();
+            var up = new UnitParser();
+            up.Append(_meterSymbol, _length);
+            up.Append(_kilgogramSymbol, _mass);
+            up.Append(_secondsSymbol, _time);
+            up.Append(_ampereSymbol, _electricCurrent);
+            up.Append(_kelvinSymbol, _temperature);
+            up.Append(_moleSymbol, _amountOfSubstance);
+            up.Append(_candelaSymbol, _luminousIntensity);
+            return up.ToString();
         }
 
         public Int16 Length
